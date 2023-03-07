ea1a9024d773a0a00a271d11b068656322977d80
Abhiram Natarajan <abhiram@pal-nat186-171-88.itap.purdue.edu>
2023-03-07 18:32:41 -0500

    Implemented Group Commit for Insert and Update

--- crud/insert.py
+++ crud/insert.py
@@ -5,29 +5,54 @@ let it happen after each successful operation.
 """
 import random
 import string
+import threading
 from memory import DatabaseStorage
 
 class Insert:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, interval: int = 0) -> None:
+        self.db = None
+        self.start_commit = False
+        self.interval = interval
 
-    def insert_one(self, database, collection_name, payload, database_location = './'):
-        # creates the database and the collection if they dont exist as well.
-        db = DatabaseStorage(database_location= database_location, database_name= database, collection_name= collection_name)
+    def writeToDisk(self):
+        self.db.write_file()
+    
+    def stop_commit(self):
+        self.runtime.cancel()
+        self.db.write_file()
+        self.start_commit = False
+        print("Commit complete")
+
+    def commit(self,):
+        self.runtime = threading.Timer(self.interval, self.commit)
+        self.runtime.start()
+        self.writeToDisk()
+
+    def insert_one(self, database: str, collection_name: str, payload: dict, database_location: str = './', group_commit: bool = False):
+        # Caching the database from the past
+        if not group_commit:
+            self.db = DatabaseStorage(database_location= database_location, database_name= database, collection_name= collection_name)
+        elif (group_commit and not self.start_commit):
+            # creates the database and the collection if they dont exist as well.
+            self.db = DatabaseStorage(database_location= database_location, database_name= database, collection_name= collection_name)
+            self.start_commit = True
+            self.commit()
+
         # Check for duplicates by Primary Key
         if "_id" not in payload:
             payload['_id']= ''.join(random.choices(string.ascii_letters + string.digits, k=38))
         # Make sure the '_id' isnt in the database already, if it is, raise Exception
-        for i, id in enumerate(db.storage['_data']):
+        for i, id in enumerate(self.db.storage['_data']):
             if payload['_id'] == id:
                 raise Exception('Key Conflict, Alter the Primary Key and try again')
-        if '_data' not in db.storage:
+        if '_data' not in self.db.storage:
             raise Exception("Issues with the collection")
-        db.storage['_data'][payload['_id']] = payload
-        db.write_file()
+        self.db.storage['_data'][payload['_id']] = payload
+        if not group_commit:
+            self.db.write_file()
         return "OK"
     
-    def insert_many(self, database, collection_name, payloads, database_location = './'):
+    def insert_many(self, database: str, collection_name: str, payloads: dict, database_location: str = './', group_commit: bool = False):
         # creates the database and the collection if they dont exist as well.
         db = DatabaseStorage(database_location= database_location, database_name= database, collection_name= collection_name)
         if '_data' not in db.storage:
@@ -40,5 +65,6 @@ class Insert:
                 if payload['_id'] == id:
                     raise Exception('Key Conflict, Alter the Primary Key and try again')
             db.storage['_data'][payload['_id']] = payload
-        db.write_file()
+        if not group_commit:
+            db.write_file()
         return "OK"
--- crud/update.py
+++ crud/update.py
@@ -4,25 +4,54 @@ Later designs should also support a filter for search and other such keys as wel
 Also, just like MongoDB, an update operation performs a replace of the document, not just updates 
 to specific fields.
 """
+import threading
 from memory import DatabaseStorage
 
 class Update:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, interval: int = 0) -> None:
+        self.db = None
+        self.interval = interval
+        self.start_commit = False
 
-    def update_one(self, database, collection_name, payload, database_location='./'):
+    def writeToDisk(self):
+        self.db.write_file()
+    
+    def stop_commit(self):
+        self.runtime.cancel()
+        self.start_commit = False
+        self.db.write_file()
+        print("Commit complete")
+
+    def commit(self,):
+        self.runtime = threading.Timer(self.interval, self.commit)
+        self.runtime.start()
+        print("Committing")
+        self.writeToDisk()
+
+    def update_one(self, database, collection_name, payload, database_location='./', group_commit: bool = False):
+        if not group_commit:
+            self.db = DatabaseStorage(database_location= database_location, database_name= database, collection_name= collection_name)
+        elif (group_commit and not self.start_commit):
+            # creates the database and the collection if they dont exist as well.
+            self.db = DatabaseStorage(database_location= database_location, database_name= database, collection_name= collection_name)
+            self.start_commit = True
+            self.commit()
         if '_id' in payload:
-            db = DatabaseStorage(database_location=database_location, database_name= database, collection_name= collection_name)
+            self.db = DatabaseStorage(database_location=database_location, database_name= database, collection_name= collection_name)
             # TODO: This forces the creation of a database and collection if they dont exist. 
             # Delete those creations in the future.
-            if db.storage['_data'] == []:
+            if self.db.storage['_data'] == []:
+                if self.start_commit:
+                    self.stop_commit()
                 raise Exception("Empty Collection")
 
-            if payload['_id'] in db.storage['_data']:
-                db.storage['_data'][payload['_id']] = payload
-                db.write_file()
+            if payload['_id'] in self.db.storage['_data']:
+                self.db.storage['_data'][payload['_id']] = payload
+                if not self.start_commit:
+                    self.db.write_file()
                 return "OK"
-
+            if self.start_commit:
+                self.stop_commit()
             raise Exception("Key not found")
 
         else:
